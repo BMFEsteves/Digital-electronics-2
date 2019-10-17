95acbe14055f5379460b5a80bedfa086a3df29e5
Bruno M. F. Esteves <bmiguel.est@gmail.com>
2019-10-17 16:46:17 +0200

    04-interrupts

--- projects/library/src/gpio.c
+++ projects/library/src/gpio.c
@@ -1,54 +1,50 @@
-# include "gpio.h"
+
+#include "gpio.h"
+
+/* Define ------------------------------------------------------------*/
 #define DDR(x) (*(&x - 1))
 
 
- // test
-/* Functions - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-void GPIO_config_output ( volatile uint8_t * reg_name , uint8_t pin_num ){
-
-* reg_name |= _BV ( pin_num ) ;
+/* Functions ---------------------------------------------------------*/
+void GPIO_config_output(volatile uint8_t *reg_name, uint8_t pin_num)
+{
+    *reg_name |= _BV(pin_num);
 }
 
-
-void GPIO_config_output ( volatile uint8_t * reg_name , uint8_t pin_num ) {
-    
-* reg_name &= ~_BV ( pin_num ) ;
-
-}
-
-void GPIO_config_input_nopull ( volatile uint8 t *reg_name , uint8 t pin num ) {
-
-    * reg_name &= _BV ( pin_num ) ;
+/*--------------------------------------------------------------------*/
+void GPIO_config_input_nopull(volatile uint8_t *reg_name, uint8_t pin_num)
+{
+    *reg_name &= _BV(pin_num);
     DDR(*reg_name) &= ~_BV(pin_num);
 }
 
-void GPIO_config_input_pullup ( volatile uint8 t *reg_name , uint8 t pin num ) {
-    
-* reg_name |= _BV ( pin_num ) ;
+/*--------------------------------------------------------------------*/
+void GPIO_config_input_pullup(volatile uint8_t *reg_name, uint8_t pin_num)
+{
+    *reg_name |= _BV(pin_num);
     DDR(*reg_name) &= ~_BV(pin_num);
 }
 
-void GPIO_write ( volatile uint8 t *reg_name , uint8 t pin num , uint8 t pin val ) {
-
-      if(pin_val==1){
-            
-            *reg_name |= _BV ( pin_num ) ;
-
-
-      }
-
-
+/*--------------------------------------------------------------------*/
+void GPIO_write(volatile uint8_t *reg_name, uint8_t pin_num, uint8_t pin_val)
+{
+    if (pin_val == PIN_LOW) {
+        *reg_name = *reg_name & ~_BV(pin_num);
+    }
+    else {
+        *reg_name = *reg_name | _BV(pin_num);
+    }
 }
 
-void GPIO_toggle ( volatile uint8 t *reg_name , uint8 t pin num ) {
-      
-      *reg_name |= _BV ( pin_num ) ;
-
-
+/*--------------------------------------------------------------------*/
+void GPIO_toggle(volatile uint8_t *reg_name, uint8_t pin_num)
+{
+    *reg_name ^= _BV(pin_num);
 }
 
-void GPIO_read ( volatile uint8 t *reg_name , uint8 t pin num ) {
-    
-  
-
+/*--------------------------------------------------------------------*/
+uint8_t GPIO_read(volatile uint8_t *reg_name, uint8_t pin_num)
+{
+    //return bit_is_clear(*reg_name, pin_num);
+    return ((*reg_name & _BV(pin_num)) >> pin_num);
 }
